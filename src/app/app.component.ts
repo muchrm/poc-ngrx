ac2f6648df4534ef517f14ed2ab09fa2ecae9c65
Pongpanot Chuaysakun <pongpanot@flowaccount.com>
2019-01-30 11:38:32 +0700

    add subscribe inside componets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Increment, Decrement, Reset } from './actions';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
-
+import {distinctUntilChanged} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,8 +13,9 @@ export class AppComponent {
 
   constructor(private store: Store<{ count: number }>) {
     this.count$ = store.pipe(select('count'));
-    // this.store.dispatch(new Reset());
-    this.count$.subscribe(count => console.log(count));
+    this.count$.pipe(distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))).subscribe(count => {
+      this.store.dispatch(new Reset());
+    });
   }
 
   increment() {
